8242c545414dea799cb682b641910354f90fb9fa
JagadishAtelier <jagadish.atelier@gmail.com>
2025-10-27 19:34:02 +0530

    first commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,8 @@
-import HeroSection from '@/Components/HomePage/HeroSection'
-import HomeAbout from '@/Components/HomePage/HomeAbout'
-import MouseTrail from '@/Components/MouseTrail'
-import Navbar from '@/Components/Navbar'
+
+import HeroSection from '../Components/HomePage/HeroSection'
+import HomeAbout from '../Components/HomePage/HomeAbout'
+import MouseTrail from '../Components/MouseTrail'
+import Navbar from '../Components/Navbar'
 import React from 'react'
 
 function Home() {
