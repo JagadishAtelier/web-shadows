b893126eac24a57d0b42f01fad140e59f5c4d45a
JagadishAtelier <jagadish.atelier@gmail.com>
2025-10-29 13:14:15 +0530

    Fix HeroSection import path for build

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,8 @@
 import React from 'react';
-import Navbar from '@/Components/Navbar';
-import HeroSection from '@/Components/HomePage/HeroSection';
-import HomeAbout from '@/Components/HomePage/HomeAbout';
 
 function Home() {
   return (
     <div>
-      <Navbar />
-      <HeroSection />
-      <HomeAbout />
     </div>
   );
 }
