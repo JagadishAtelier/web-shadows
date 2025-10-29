de7596ec8b076c6885d753720799b2e9a097779b
JagadishAtelier <jagadish.atelier@gmail.com>
2025-10-29 13:00:14 +0530

    Fix HeroSection import path for build

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import MouseTrail from "@/Components/MouseTrail.jsx";
 import Navbar from "@/Components/Navbar.jsx";
 import HeroSection from "@/Components/HomePage/HeroSection.jsx";
 import HomeAbout from "@/Components/HomePage/HomeAbout.jsx";
+import MouseTrail from '@/Components/MouseTrail';
 
 function Home() {
   return (
